29f8507976baecfdc7950e6d3af2a42d11c8bf0e
Ending2015a <joe85812@yahoo.com.tw>
2017-11-01 00:13:04 +0800

    update simp_run script

--- simp_run.sh
+++ simp_run.sh
@@ -14,6 +14,9 @@ set -x
 time srun -p batch -N 4 -n 8 ./ms_seq 1 $low $up $left $right $width $height seq.png
 time srun -p batch -N 4 -n 8 ./ms_mpi_static 1 $low $up $left $right $width $height mpi.png
 time srun -p batch -N 4 -n 8 ./ms_mpi_dynamic 1 $low $up $left $right $width $height dyn.png
+time srun -p batch -N 1 -n 1 -c 8 ./ms_omp 8 $low $up $left $right $width $height omp.png
+
 
 hw2-diff seq.png mpi.png
 hw2-diff seq.png dyn.png
+hw2-diff seq.png omp.png
